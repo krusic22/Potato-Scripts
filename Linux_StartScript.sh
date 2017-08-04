fb6dde9e8648794dcd5b78c800e1a2d811a80384
Kristjan Krušič <kristjan.krusic@gmail.com>
2017-08-04 18:49:09 +0200

    Added more parameters
    Hope this helps some people out...

--- Linux_StartScript.sh
+++ Linux_StartScript.sh
@@ -1,14 +1,52 @@
 #!/bin/sh
 #By Kristjan Krusic aka. krusic22
 ###Don't forget to adjust the variables to your own needs!
+###We only support Java 8!
+#Less time you spend in GC the better the performance! But higher the Ram usage.
 #Note: 1G = 1024M
-STARTRAM=128M 
-MAXRAM=1G
-JARNAME=spigot.jar
+STARTRAM=128M           #USE VALUES IN M! Setting this the same value as MAXRAM can help sometimes...
+MAXRAM=1024M            #USE VALUES IN M!
+JARNAME=spigot.jar        #Spigoterino.Jar
+EXP=false                        #Enable experimental stuff... It can cause problems just so you know 
+#Normal Parameters
+PARMS="
+-XX:+AlwaysPreTouch
+-XX:+DisableExplicitGC
+-XX:+UseG1GC
+-XX:+UnlockExperimentalVMOptions
+-XX:+AggressiveOpts"
+#Set ParallelGCThreads same as Logical CPU cores MAX 8 and ConcGCThreads to one fourth of that.
+THREADS="
+-XX:ParallelGCThreads=8
+-XX:ConcGCThreads=2"
+#G1 optimizations...
+GONE="-XX:MaxGCPauseMillis=75
+-XX:TargetSurvivorRatio=90
+-XX:G1NewSizePercent=50
+-XX:G1MaxNewSizePercent=80
+-XX:InitiatingHeapOccupancyPercent=10
+-XX:G1MixedGCLiveThresholdPercent=50
+-XX:G1HeapWastePercent=7
+"
+#Experimental options... Use at your own risk
+if ("$EXP" = true ) then
+PARMS = "-d64 -server -XX:+UseLargePagesInMetaspace -XX:LargePageSizeInBytes=4M -XX:+UseLargePages $PARMS"
+fi
+
+### Auto Jar Updater. It works but it's not the best.
+#wget InsertURLToJarHere -O $JARNAME
+###
+
 ### You can stop the script by pressing CTRL+C multiple times.
 while true
 do
-java -Xms$STARTRAM -Xmx$MAXRAM  -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=45 -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -jar $JARNAME
-echo "Server Restarting!"
-sleep 5
+java -Xms$STARTRAM -Xmx$MAXRAM $PARMS $THREADS $GONE -jar $JARNAME
+echo "Server will restart in 3s!"
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 1
+echo "Starting!"
 done
