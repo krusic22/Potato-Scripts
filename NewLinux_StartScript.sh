2c00825d3986c5adc443474c33bbe76b98e00826
Kristjan Krušič <kristjan.krusic@gmail.com>
2020-02-25 17:29:16 +0100

    Updated startup scripts.

--- NewLinux_StartScript.sh
+++ NewLinux_StartScript.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 #By Kristjan Krusic aka. krusic22
 #Don't forget to adjust the variables to your own needs!
-#This script is optimised for Java 8! Get the latest Java 8 from AdoptJDK.
+#This script is optimised for Java 10+! Get the latest Java from AdoptJDK.
 #Note: This script is optimised for Speed and will not lower ram usage!
 #Less time you spend in GC the better the performance! But higher the ram usage.
 #Note: 1G = 1024M
 STARTRAM=128            #USE VALUES IN M! Setting this the same value as MAXRAM can help sometimes...
 MAXRAM=1024             #USE VALUES IN M!
-JARNAME=paper.jar       #paper.Jar
-IS64=true               #Disable this if you don't have 64 bit Java installed
-EXP=false               #Enable experimental stuff... It can cause problems just so you know
-LP=false                #Enable only if you have Large/Huge Pages enabled.
+JARNAME=paper.jar       #paper.jar
+EXP=true                #Enable experimental stuff... It can cause problems, personally didn't notice any.
+LP=false                #Enable only if you have Large/Huge Pages enabled, transparent  pages are recommended for normal users. 
 #Normal Parameters
 PARMS="
 -server
--XX:+AlwaysPreTouch
 -XX:+UnlockExperimentalVMOptions
--XX:+AggressiveOpts
 -XX:+UseGCOverheadLimit
 -XX:+OptimizeStringConcat
--XX:+UseFastAccessorMethods
 -XX:MaxHeapFreeRatio=80
 -XX:MinHeapFreeRatio=40"
 #G1 optimizations...
@@ -37,28 +33,22 @@ GONE="
 #Experimental options... Use at your own risk
 if [ "$EXP" = true ]; then
 echo "You have enabled Experimental Options! Use at your own risk!"
-PARMS="-XX:+ExitOnOutOfMemoryError -XX:+UseXMMForArrayCopy -XX:+UseXmmI2D -XX:+UseXmmI2F -XX:+UseNewLongLShift -XX:+UseFastEmptyMethods -XX:+UseAdaptiveGCBoundary  $PARMS"
+PARMS="-XX:+ExitOnOutOfMemoryError -XX:+UseXMMForArrayCopy -XX:+UseXmmI2D -XX:+UseXmmI2F -XX:+UseNewLongLShift -XX:+UseAdaptiveGCBoundary $PARMS"
 fi
 #Large Pages config
 if [ "$LP" = true ]; then
 PARMS="-XX:+UseTransparentHugePages -XX:+UseLargePagesInMetaspace -XX:+UseLargePagesInMetaspace -XX:LargePageSizeInBytes=2M -XX:+UseLargePages $PARMS"
 fi
-#64Bit Java Toggle
-if [ "$IS64" = true ]; then
-PARMS="-d64 $PARMS"
-fi
 #G1 Is only usefull when you have more then 4GB of ram...
 if [ "$MAXRAM" -ge '1024' ]; then
 PARMS="-XX:+DisableExplicitGC -XX:-UseParallelGC -XX:-UseParallelOldGC -XX:+UseG1GC $PARMS $GONE"
 fi
-
 ### Auto Jar Updater. It works but it's not the best.
 JARLINK=https://papermc.io/api/v1/paper/1.15.2/latest/download
 function UpdateJar {
 echo "Updating Jar..."
 wget $JARLINK -O $JARNAME 2>/dev/null || curl $JARLINK > $JARNAME
 }
-
 ### You can stop the script by pressing CTRL+C multiple times.
 #Move the "UpdateJar" to where you like
 while true
