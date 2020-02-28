96d8c3eb49067125b32478cb764ec298d19e712d
Kristjan Krušič <kristjan.krusic@gmail.com>
2020-02-28 23:35:32 +0100

    Revised comments.

--- Linux_StartScript.sh
+++ Linux_StartScript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #By Kristjan Krusic aka. krusic22
-#Don't forget to adjust the variables to your own needs!
-#This script is optimized for Java 11+! Get the latest Java from AdoptJDK or ZuluJDK for ARM.
-#Note: This script is optimized for Speed and will not lower ram usage!
-#Less time you spend in GC the better the performance! But higher the ram usage.
+#Don't forget to adjust the variables according your own needs!
+#This is an Java 11+ optimised script! Get the most recent AdoptJDK or ZuluJDK for ARM.
+#This script is speed-optimized and won't reduce ram use!
+#Less time spent on GC, the better the performance, but possibly higher ram usage.
 #Note: 1G = 1024M
-STARTRAM=128            #USE VALUES IN M! Setting this the same value as MAXRAM can help sometimes...
+STARTRAM=128            #USE VALUES IN M! Sometimes setting this to the same value as MAXRAM can help performance.
 MAXRAM=1024             #USE VALUES IN M!
 JARNAME=paper.jar       #paper.jar
 ###Only use one garbage collector!
@@ -14,16 +14,16 @@ SHEN=false              #Use ShenandoahGC.
 ZGC=false               #The Z Garbage Collector
 ###
 #Experimental stuff. Good luck.
-EXP=false               #Enable experimental stuff... It can cause problems, personally didn't notice any.
-LP=false                #Enable only if you have Large/Huge Pages enabled, transparent  pages are recommended for normal users.
+EXP=false               #Enable experimental stuff... It might cause unexpected problems but I haven't noticed any yet.
+LP=false                #Enable only if you have Large/Huge Pages enabled, transparent pages are recommended for regular users.
 X86=false               #Flags that should only work on X86.
 #
-###Unused Parameters, you might want to use some depending on your setup, copy the parameters under Normal Parameters,
-###due to IgnoreUnrecognizedVMOptions being set, unknown/invalid options will be ignored instead of stopping the JVM.
-#-XX:ActiveProcessorCount=4 #This should limit the CPU Core usage, but it's more of a suggestion than a limitation.
-#-Xlog:gc*:file=GC.log #This will log GC to a file called GC.log, can be used to debug GC, replace 'file=GB.log' with 'stdout' if you want logging to the console. Other options you can change/add pid,level,tags,...
+#Unused Parameters, you might want to use some of them depending on your configuration, copy the parameters under Normal Parameters,
+#since IgnoreUnrecognizedVMOptions is set, unknown / invalid options will be ignored instead of stopping the JVM.
+#-XX:ActiveProcessorCount=4 #This should restrict the use of CPU cores, although this is more of a suggestion than a constraint.
+#-Xlog:gc*:file=GC.log #This will log GC to a file called GC.log, which can be used to debug GC, replace 'file=GB.log' with 'stdout' if you want logging to the console. Other options you can change/add pid,level,tags,...
 #-Xlog:gc*:file=GC.log:time,uptimemillis,tid #Same as above, but with local time, uptime/runtime and thread IDs.
-#-Xlog:gc*=debug:file=GC.log:time,uptimemillis,tid #Same as above but with extra debug. Warning: This will grow fast!
+#-Xlog:gc*=debug:file=GC.log:time,uptimemillis,tid #Same as above, but with some extra debug. Warning: This is going to grow quickly!
 ###
 #Normal Parameters
 PARMS="
@@ -46,7 +46,7 @@ GONEP="
 -XX:MinHeapFreeRatio=40
 -XX:GCTimeRatio=12
 -XX:GCTimeLimit=98"
-#Shenandoah options that might be worth looking into, some options here only got added in JDK12, currently just default values from AdoptJDK13.
+#Shenandoah options that might be worth looking into, some of the options only got added in JDK12+, currently set to default values from AdoptJDK13.
 SHENP="
 -XX:ShenandoahAllocSpikeFactor=5
 -XX:ShenandoahControlIntervalAdjustPeriod=1000
@@ -64,7 +64,7 @@ SHENP="
 -XX:-ShenandoahOptimizeStableFinals
 -XX:+ShenandoahOptimizeStaticFinals
 "
-#Experimental options... Use at your own risk
+#Experimental options... Use at your own risk!
 if [ "$EXP" = true ]; then
 echo "You have enabled Experimental Options! Use at your own risk!"
 PARMS="$PARMS -XX:+ExitOnOutOfMemoryError -XX:+AlwaysPreTouch -XX:+UseAdaptiveGCBoundary -XX:-DontCompileHugeMethods -XX:+TrustFinalNonStaticFields -XX:+UseFastUnorderedTimeStamps "
@@ -85,19 +85,19 @@ fi
 if [ "$ZGC" = true ]; then
 PARMS="$PARMS -XX:+DisableExplicitGC -XX:-UseParallelGC -XX:-UseParallelOldGC -XX:-UseG1GC -XX:+UseZGC"
 fi
-#Experimental X86 abomination, some flags might not be ARCH specific, so could also work on other platforms.
+#Experimental X86 abomination, some of the flags may not be ARCH specific, so they could work on other platforms as well.
 if [ "$X86" = true ]; then
 PARMS="$PARMS -XX:+UseCMoveUnconditionally -XX:+UseFPUForSpilling -XX:+UseNewLongLShift -XX:+UseVectorCmov -XX:+UseXMMForArrayCopy -XX:+UseXmmI2D -XX:+UseXmmI2F -XX:+UseXmmLoadAndClearUpper -XX:+UseXmmRegToRegMoveAll"
 fi
 #
-### Auto Jar Updater. It works but it's not the best.
+###Auto Jar Updater. It works but it's not the best.
 JARLINK=https://papermc.io/api/v1/paper/1.15.2/latest/download
 function UpdateJar {
 echo "Updating Jar..."
 wget $JARLINK -O $JARNAME 2>/dev/null || curl $JARLINK > $JARNAME
 }
-### You can stop the script by pressing CTRL+C multiple times.
-#Move the "UpdateJar" to where you like
+###You can stop this script by pressing CTRL+C multiple times.
+#Move the "UpdateJar" to where you want it.
 while true
 do
 #UpdateJar
