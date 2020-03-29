626f139c23ddc3b89736cef8c23e11e9da40c4b0
Kristjan Krušič <kristjan.krusic@gmail.com>
2020-03-29 19:54:00 +0200

    Switched to Parallel ZSTD.
    Quick tests show a small size reduction and speed boost over Parallel GZ at the expense of higher memory usage (x1.8 in my case).

--- MEGABackup.sh
+++ MEGABackup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #By Kristjan Krusic aka. krusic22
 ###Don't forget to adjust the variables to your own needs!
-#This script needs megatools and pigz installed.
+#This script needs megatools and pzstd installed.
 EMAIL=
 PASSWORD=
 #NO ENDING SLASH ////////////
@@ -12,7 +12,7 @@ LOCALBACKUPNAME=backup
 #
 TIME=$(date +"%d_%m_%Y")
 #
-tar -I pigz -zcvf $LOCALBACKUPNAME-$TIME.tar.gz $FULLPATHTOBACKUPDIRECTORY
-megaput --path $REMOTEPATH/$REMOTEBACKUPNAME-$TIME.tar.gz  $LOCALBACKUPNAME-$TIME.tar.gz --username $EMAIL --password $PASSWORD
+tar -I pzstd -zcvf $LOCALBACKUPNAME-$TIME.tar.zst $FULLPATHTOBACKUPDIRECTORY
+megaput --path $REMOTEPATH/$REMOTEBACKUPNAME-$TIME.tar.zst  $LOCALBACKUPNAME-$TIME.tar.zst --username $EMAIL --password $PASSWORD
 #Uncomment if you want to remove the local BACKUP.
-#rm $LOCALBACKUPNAME-$TIME.tar.gz
+#rm $LOCALBACKUPNAME-$TIME.tar.zst
